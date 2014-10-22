138c41c8aee22ca86b2f2ec8e2ddb671c063cc85
Anthony Clay <anthony.clay@zarthcode.com>
2014-10-21 21:43:03 -0500

    Manual edit to gpio.c paired w/ioc correction.

--- Configuration/Src/gpio.c
+++ Configuration/Src/gpio.c
@@ -261,7 +261,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PD4 */
   GPIO_InitStruct.Pin = GPIO_PIN_4;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
